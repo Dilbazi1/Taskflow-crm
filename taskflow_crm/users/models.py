43e98bd11f99f2fa1f1b399b5727efcce21f65d1
dilbazi <you@example.com>
2026-04-16 08:46:11 +0200

    Initial commit

---
+++ taskflow_crm/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
